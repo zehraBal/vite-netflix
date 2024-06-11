dc5672d9c984dedde3b16509c195e6f4a0b7b888
zehraBal <fz.bal00@gmail.com>
2024-06-11 14:36:37 +0300

    login form created

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,26 @@
+import { useForm } from "react-hook-form";
+
 export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: { email: "", password: "" },
+  });
+  const handleSubmit = () => {};
   return (
     <div className="loginPage-container">
-      <h1>Login</h1>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <input type="email" {...register("email")} />
+          <input type="password" {...register("password")} />
+          <button type="submit">Login</button>
+          <h3>OR</h3>
+          <div>Redeem Login Code</div>
+          <div>Forgot your password?</div>
+        </form>
+      </div>
     </div>
   );
 }
