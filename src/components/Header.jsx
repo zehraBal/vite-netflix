a2593e655c66dc36c9c459735c7afe74a635cee4
zehraBal <fz.bal00@gmail.com>
2024-06-12 01:16:49 +0300

    The header and mainpage were edited

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,22 @@
 import { useHistory } from "react-router-dom";
 export default function Header() {
   const history = useHistory();
-  /* const handleClick = (e) => {
-    const { name } = e.target;
-    if (name === "signIn") {
-      history.push("/login");
-    } else if (name === "signUp") {
-      history.push("/signUp");
-    }
-  };*/
   const handleLogin = () => {
     history.push("/login");
   };
   return (
     <div className="header">
-      <img src="/src/assets/Netflix_Logo_RGB.png" alt="logo" />
+      <img
+        src="/src/assets/Netflix_Logo_RGB.png"
+        alt="logo"
+        onClick={() => {
+          history.push("/");
+        }}
+      />
       <div className="headerButton-container">
         <button className="headerButton" onClick={handleLogin}>
           Sign In
         </button>
-        <button className="headerButton">Get Started</button>
       </div>
     </div>
   );
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,19 @@
+import Header from "../components/Header";
+
 export default function MainPage() {
   return (
     <div className="mainPage-container">
-      <h1>Sınırsız film, dizi ve çok daha fazlası</h1>
-      <h3>İstediğiniz yerde izleyin. İstediğiniz zaman iptal edin </h3>
-      <h4>
-        İzlemeye hazır mısınız? Üye olmak ya da hesabınıza tekrar ulaşmak için
-        tek yapmanız gereken e-posta adresinizi girmek.
-      </h4>
-      <input type="text" />
-      <button></button>
+      <div className="main-content">
+        <h1>Unlimited movies, TV shows, and more</h1>
+        <h3>Watch anywhere. Cancel anytime. </h3>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>
+        <div className="mainInputField">
+          <input className="main-input" type="text" placeholder="Email" />
+          <button className="headerButton">Get Started {">"}</button>
+        </div>
+      </div>
     </div>
   );
 }
